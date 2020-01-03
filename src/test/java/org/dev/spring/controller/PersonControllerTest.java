32983d32e64c5e3f06e065c1f585c44c2095a397
Amol Shinde <ashinde-v@hypergrid.com>
2020-01-03 11:31:29 +0530

    added test case and add peocedure modify pom

---
+++ src/test/java/org/dev/spring/controller/PersonControllerTest.java
@@ -0,0 +1,13 @@
+package org.dev.spring.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PersonControllerTest {
+
+	@Test
+	public void getAllUser() {
+		boolean value = true;
+		Assert.assertEquals(true, value);
+	}
+}
