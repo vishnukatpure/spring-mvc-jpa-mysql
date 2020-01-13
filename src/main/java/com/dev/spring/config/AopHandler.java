d5e9cb6ce185aefd0f0920abb7f3dcf62f34be79
Vishnu Katpure <katpurev@gmail.com>
2020-01-13 13:35:23 +0530

    add response

--- src/main/java/com/dev/spring/config/AopHandler.java
+++ src/main/java/com/dev/spring/config/AopHandler.java
@@ -4,13 +4,15 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Configuration;
 
+import com.dev.spring.dto.ResponseDTO;
+import com.dev.spring.enums.StatusEnum;
+
 @Aspect
 @Configuration
 public class AopHandler {
 
 	@AfterThrowing(pointcut = "execution(* com.dev.spring.services.*.*(..))", throwing = "ex")
-	private void afterThrow(Exception ex) {
-		System.out.println("in AfterThrowing");
-		System.out.println(ex.getMessage());
+	private ResponseDTO afterThrow(Exception ex) {
+		return new ResponseDTO().message(ex.getMessage()).object(null).status(StatusEnum.EXCEPTION_OCCIRED);
 	}
 }
